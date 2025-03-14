b89f02fd8edfe7f8caec4d134b33e1ddcd52301b
Rethanya <rethanya318@gmail.com>
2025-03-14 16:06:23 +0530

    setup socketio

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,28 @@
 import express from "express";
-import { createServer} from "node:http";
-import {Server} from "socket.io";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
+import { connectToSocket } from "./controllers/socketManager.js";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = connectToSocket(server);
 
-app.set("port", (process.env.PORT || 8000))
-app.get("/home", (req,res) => {
-    return res.json({"hello": "World"})
-})
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({limit: "40kb "}));
+app.use(express.urlencoded({limit: "40kb", extended: true}))
 
-const start = async() => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://rethanya888:vHn6fzjH5qClw4Kx@cluster0.zex9c.mongodb.net/vibio?retryWrites=true&w=majority&appName=Cluster0")
-    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("Listening on port 8000")
-    })
-}
+const start = async () => {
+  app.set("mongo_user");
+  const connectionDb = await mongoose.connect(
+    "mongodb+srv://rethanya888:vHn6fzjH5qClw4Kx@cluster0.zex9c.mongodb.net/vibio?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+  server.listen(app.get("port"), () => {
+    console.log("Listening on port 8000");
+  });
+};
 
 start();
