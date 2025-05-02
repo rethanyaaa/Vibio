4ef28a2c59d8a70795cc48277f18fa830d6f41cf
Rethanya <rethanya318@gmail.com>
2025-05-02 16:46:23 +0530

    Setup videomeet

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,8 +1,9 @@
-import { Server } from "socket.io";
+import { Server } from "socket.io"
 
-let connections = {};
-let messages = {};
-let timeOnline = {};
+
+let connections = {}
+let messages = {}
+let timeOnline = {}
 
 export const connectToSocket = (server) => {
     const io = new Server(server, {
@@ -14,90 +15,105 @@ export const connectToSocket = (server) => {
         }
     });
 
+
     io.on("connection", (socket) => {
+
+        console.log("SOMETHING CONNECTED")
+
         socket.on("join-call", (path) => {
+
             if (connections[path] === undefined) {
-                connections[path] = [];
+                connections[path] = []
             }
-            connections[path].push(socket.id);
+            connections[path].push(socket.id)
 
             timeOnline[socket.id] = new Date();
 
+            // connections[path].forEach(elem => {
+            //     io.to(elem)
+            // })
+
             for (let a = 0; a < connections[path].length; a++) {
-                io.to(connections[path][a]).emit("user-joined", socket.id, connections[path]);
+                io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
             }
 
             if (messages[path] !== undefined) {
                 for (let a = 0; a < messages[path].length; ++a) {
-                    io.to(socket.id).emit(
-                        "chat-message",
-                        messages[path][a]['data'],
-                        messages[path][a]['sender'],
-                        messages[path][a]['socket-id-sender']
-                    );
+                    io.to(socket.id).emit("chat-message", messages[path][a]['data'],
+                        messages[path][a]['sender'], messages[path][a]['socket-id-sender'])
                 }
             }
-        });
+
+        })
 
         socket.on("signal", (toId, message) => {
             io.to(toId).emit("signal", socket.id, message);
-        });
+        })
 
         socket.on("chat-message", (data, sender) => {
-            const [matchingRoom, found] = Object.entries(connections).reduce(
-                ([room, isFound], [roomKey, roomValue]) => {
+
+            const [matchingRoom, found] = Object.entries(connections)
+                .reduce(([room, isFound], [roomKey, roomValue]) => {
+
+
                     if (!isFound && roomValue.includes(socket.id)) {
                         return [roomKey, true];
                     }
+
                     return [room, isFound];
-                },
-                ['', false]
-            );
+
+                }, ['', false]);
 
             if (found === true) {
                 if (messages[matchingRoom] === undefined) {
-                    messages[matchingRoom] = [];
+                    messages[matchingRoom] = []
                 }
 
-                messages[matchingRoom].push({
-                    'sender': sender,
-                    'data': data,
-                    'socket-id-sender': socket.id
-                });
-
-                console.log("message", matchingRoom, ":", sender, data);
+                messages[matchingRoom].push({ 'sender': sender, "data": data, "socket-id-sender": socket.id })
+                console.log("message", matchingRoom, ":", sender, data)
 
                 connections[matchingRoom].forEach((elem) => {
-                    io.to(elem).emit("chat-message", data, sender, socket.id);
-                });
+                    io.to(elem).emit("chat-message", data, sender, socket.id)
+                })
             }
-        });
+
+        })
 
         socket.on("disconnect", () => {
-            var diffTime = Math.abs(new Date() - new Date(timeOnline[socket.id]));
 
-            var key;
+            var diffTime = Math.abs(timeOnline[socket.id] - new Date())
 
-            for (const [k, v] of Object.entries(connections)) {
+            var key
+
+            for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
+
                 for (let a = 0; a < v.length; ++a) {
                     if (v[a] === socket.id) {
-                        key = k;
+                        key = k
 
                         for (let a = 0; a < connections[key].length; ++a) {
-                            io.to(connections[key][a]).emit('user-left', socket.id);
+                            io.to(connections[key][a]).emit('user-left', socket.id)
                         }
 
-                        var index = connections[key].indexOf(socket.id);
-                        connections[key].splice(index, 1);
+                        var index = connections[key].indexOf(socket.id)
 
-                        if (connections[key] && connections[key].length === 0) {
-                            delete connections[key];
+                        connections[key].splice(index, 1)
+
+
+                        if (connections[key].length === 0) {
+                            delete connections[key]
                         }
                     }
                 }
+
             }
-        });
-    });
+
+
+        })
+
+
+    })
+
 
     return io;
-};
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/landing'
 import Authentication from './pages/authentication'
+import { AuthProvider } from './contexts/AuthContext'
+import VideoMeetComponent from './pages/videoMeet'
 
 
 function App() {
@@ -11,10 +13,13 @@ function App() {
   return (
     <>
        <Router>
+        <AuthProvider>
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
           <Route path='/auth' element={<Authentication/>}/>
+          <Route path='/:url' element={<VideoMeetComponent/>}/>
         </Routes>
+        </AuthProvider>
        </Router>
     </>
   )
