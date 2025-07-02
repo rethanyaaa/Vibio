662a5437adc400ee73ec9aa3e1d6e81810304b38
Rethanya <rethanya318@gmail.com>
2025-07-02 12:18:39 +0530

    complete videomeet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
           <Route path='/auth' element={<Authentication/>}/>
+           <Route path='/home' element={<HomeComponent />} />
           <Route path='/:url' element={<VideoMeetComponent/>}/>
         </Routes>
         </AuthProvider>
